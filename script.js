499642e50395bad05b44eb802f549ab14d2125ac
cybermicheal47 <cybermicheal47@gmail.com>
2023-05-14 09:44:57 -0400

    made all console log visible on screen and made buttons

--- script.js
+++ script.js
@@ -1,48 +1,62 @@
 const randompick = ["Rock", "Paper", "Scissors"];
 
-function playRound(playerSelection, computerSelection, randompick) {
-  // /player and computer if else if else statement 
+function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    return `It's a tie! you both picked ${playerSelection}`;
- } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return `It's a tie! You both picked ${playerSelection}`;
+  } else if (playerSelection === "rock" && computerSelection === "Scissors") {
     return "You win! Rock beats Scissors";
- } else if (playerSelection === "paper" && computerSelection === "rock") {
+  } else if (playerSelection === "paper" && computerSelection === "Rock") {
     return "You win! Paper beats Rock";
- } else if (playerSelection === "scissors" && computerSelection === "paper") {
+  } else if (playerSelection === "scissors" && computerSelection === "Paper") {
     return "You win! Scissors beats Paper";
- } else {
+  } else {
     return `You lose! ${computerSelection} beats ${playerSelection}`;
- }
+  }
 }
 
-//random pick for the computer
 function getComputerChoice(randompick) {
   const randompick_length = randompick.length;
   const value = Math.floor(Math.random() * randompick_length);
   return randompick[value];
 }
 
-//For loop that plays multiple rounds  
 let playerScore = 0;
 let computerScore = 0;
 
-for (let i = 0; i < 1000; i++) {
-  let playerChoice = prompt("Rock, paper, or scissors?").toLowerCase();
-  const computerSelection = getComputerChoice(randompick);
-  let roundResult = playRound(playerChoice, computerSelection, randompick);
-  console.log(roundResult);
-  // gameScore(roundResult);
+function updateScores(roundResult) {
   if (roundResult.includes("win")) {
     playerScore++;
   } else if (roundResult.includes("lose")) {
     computerScore++;
   }
-  console.log("Your score is " + playerScore);
-  console.log("The computer's score is " + computerScore);
+  document.getElementById("player-score").textContent = "Your score is " + playerScore;
+  document.getElementById("computer-score").textContent = "The computer's score is " + computerScore;
 
   if (playerScore === 5 || computerScore === 5) {
-    break;
+    document.getElementById("game-over").textContent = "Game over!";
   }
 }
 
-console.log("Game over!");
+function buttonClickHandler(playerChoice) {
+  const computerSelection = getComputerChoice(randompick);
+  const roundResult = playRound(playerChoice, computerSelection);
+  document.getElementById("round-result").textContent = roundResult;
+  updateScores(roundResult);
+
+
+
+  if (playerScore === 5 || computerScore === 5) {
+    rockBtn.disabled = true;
+    paperBtn.disabled = true;
+    scissorsBtn.disabled = true;
+    document.getElementById("restart-text").textContent = "Reload the page to start the game again";
+  }
+}
+
+const rockBtn = document.getElementById('rock-btn');
+const paperBtn = document.getElementById('paper-btn');
+const scissorsBtn = document.getElementById('scissors-btn');
+
+rockBtn.addEventListener('click', () => buttonClickHandler('rock'));
+paperBtn.addEventListener('click', () => buttonClickHandler('paper'));
+scissorsBtn.addEventListener('click', () => buttonClickHandler('scissors'));
